c1dbab7f730b0aeb44e53adf5492593677a9b26c
kyle <kyle@prouty.io>
2018-02-10 23:08:43 -0800

    all features implemented. uploaded to gcloud. I think its done

--- OAuth2/main.py
+++ OAuth2/main.py
@@ -2,9 +2,10 @@
 # Winter 2018
 # CS 496
 from __future__ import print_function
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, session, render_template, request, redirect, g
 from google.appengine.api import urlfetch
 import urllib
+import uuid
 import sys
 import json
 
@@ -18,29 +19,36 @@ def index():
 
 @app.route("/startflow/", methods=['GET'])
 def startflow():
-	url = url_start_oauth()
+	state = random_state()
+	url = url_start_oauth(state)
 	return redirect(url)
 
 
-def url_start_oauth():
+def url_start_oauth(random_state):
 	url = "https://accounts.google.com/o/oauth2/v2/auth?"
 	code = "response_type=code"
 	c_id = "&client_id=1073799262782-1glid3shm3a3jgrfn54inafn9qln40pa.apps.googleusercontent.com"
-	redirect = "&redirect_uri=http://localhost:8080/oauth2callback"
+	redirect = "&redirect_uri=https://clouddev-194405.appspot.com/oauth2callback"
+	#redirect = "&redirect_uri=http://localhost:8080/oauth2callback"
 	scope = "&scope=email"
-	state = "&state=secret"
+	state = "&state="+random_state
 	return url+code+c_id+redirect+scope+state
+
+
+
+def random_state():
+	state = uuid.uuid4()
+	return str(state).replace("-","") 
 
 
 
 @app.route('/oauth2callback', methods=['GET', 'POST'])
 def callback():
+	state = request.args['state']
 	code = request.args['code']
 	post_result = post_return_token(code)
 	user_data = get_user_data(post_result['access_token'])
-	print(user_data, file=sys.stderr)
-	#return str(user_data)
-	return render_template('userpage.html', user_data=user_data)
+	return render_template('userpage.html', user_data=user_data, state=state)
 
 
 
@@ -48,7 +56,8 @@ def post_return_token(oauth_code):
 	payload = {'code':oauth_code,
 			'client_id':'1073799262782-1glid3shm3a3jgrfn54inafn9qln40pa.apps.googleusercontent.com',
 			'client_secret':'NwTKpmx_SZnZP-FyZHnVH2Md',
-			'redirect_uri':'http://localhost:8080/oauth2callback',
+			#'redirect_uri':'http://localhost:8080/oauth2callback',
+			'redirect_uri':'https://clouddev-194405.appspot.com/oauth2callback',
 			'grant_type':'authorization_code'}
 	data = urllib.urlencode(payload)
 	headers = {'Content-Type': 'application/x-www-form-urlencoded'}
