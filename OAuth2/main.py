2d922e0f23440b23a0d4487c9543957ce5967aed
kyle <kyle@prouty.io>
2018-02-10 20:40:18 -0800

    oauth2 flow working. working on frontend stuff now to finish up

--- OAuth2/main.py
+++ OAuth2/main.py
@@ -1,12 +1,12 @@
+# Kyle Prouty
+# Winter 2018
+# CS 496
 from __future__ import print_function
-import sys
 from flask import Flask, render_template, request, redirect, session
 from google.appengine.api import urlfetch
 import urllib
-# from webapp2_extras import sessions
-# from webapp2 import redirect
+import sys
 import json
-
 
 app = Flask(__name__)
 
@@ -18,11 +18,11 @@ def index():
 
 @app.route("/startflow/", methods=['GET'])
 def startflow():
-	url = url_get_oauth()
+	url = url_start_oauth()
 	return redirect(url)
 
 
-def url_get_oauth():
+def url_start_oauth():
 	url = "https://accounts.google.com/o/oauth2/v2/auth?"
 	code = "response_type=code"
 	c_id = "&client_id=1073799262782-1glid3shm3a3jgrfn54inafn9qln40pa.apps.googleusercontent.com"
@@ -35,29 +35,33 @@ def url_get_oauth():
 
 @app.route('/oauth2callback', methods=['GET', 'POST'])
 def callback():
-	#print(request.args['code'], file=sys.stderr)
-	if request.methods == 'GET':
-		code = request.args['code']
-		data = urllib.urlencode( post_token_data(code) )
-		headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-		result = urlfetch.fetch(url='https://www.googleapis.com/oauth2/v4/token',
-								payload=data,
-								method=urlfetch.POST,
-								headers=headers)
-		session['token'] = result
-		print(result.content, file=sys.stderr)
-
-	if request.method == 'POST':
-		return request.content
+	code = request.args['code']
+	post_result = post_return_token(code)
+	user_data = get_user_data(post_result['access_token'])
+	print(user_data, file=sys.stderr)
+	return user_data
 
 
 
-def post_token_data(oauth_code):
-	data = {'code':oauth_code,
+def post_return_token(oauth_code):
+	payload = {'code':oauth_code,
 			'client_id':'1073799262782-1glid3shm3a3jgrfn54inafn9qln40pa.apps.googleusercontent.com',
 			'client_secret':'NwTKpmx_SZnZP-FyZHnVH2Md',
 			'redirect_uri':'http://localhost:8080/oauth2callback',
 			'grant_type':'authorization_code'}
-	return data
+	data = urllib.urlencode(payload)
+	headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+	result = urlfetch.fetch(url='https://www.googleapis.com/oauth2/v4/token',
+							payload=data,
+							method=urlfetch.POST,
+							headers=headers)
+	return json.loads(result.content)
 
 
+
+def get_user_data(token):
+	headers = {'Authorization':"Bearer "+token}
+	result = urlfetch.fetch(url='https://www.googleapis.com/plus/v1/people/me',
+								method=urlfetch.GET,
+								headers=headers)
+	return json.loads(result.content)
